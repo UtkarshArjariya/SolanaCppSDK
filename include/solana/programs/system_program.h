9a7a7d8680b49bfa52199f88938e13c77c04f587
Utkarsh Arjariya <utkarsharjariya188@gmail.com>
2026-01-16 14:30:00 -0600

    refactor PublicKey: use libsodium for isOnCurve validation

--- include/solana/programs/system_program.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "solana/instruction.h"
-#include "solana/publicKey.h"
-#include <cstdint>
-namespace solana { namespace programs {
-static const char *const SYSTEM_PROGRAM_ID = "11111111111111111111111111111111";
-Instruction transfer(const PublicKey &from,const PublicKey &to,uint64_t lamports);
-}} // namespace solana::programs
